7d0492fdf2d2e3f5ad75baf6ac0aefa934119988
Mark Wang <wangyouan0629@hotmail.com>
2015-10-05 10:52:13 +0800

    Add array exercise

---
+++ src/javabasic/ArrayExercise.java
@@ -0,0 +1,76 @@
+package javabasic;
+
+import java.util.Formatter;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ArrayExercise {
+	
+	public void gradesAverage() {
+		int[] gradesRecord;
+		int numOfStud;
+		int gradeSum = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number of students: ");
+		while (true) {
+			
+			try {
+				numOfStud = in.nextInt();
+				break;
+				
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid num, try again...");
+			}
+		}
+		
+		gradesRecord = new int[numOfStud];
+		
+		for (int i = 0; i < numOfStud; i++) {
+			while (true) {
+				System.out.printf("Enter the grade for student %d: ", i+1);
+				try {
+					gradesRecord[i] = in.nextInt();
+					if (gradesRecord[i] < 0 || gradesRecord[i] > 100) {
+						System.out.println("Invalid grade, try again...");
+					} else {
+						gradeSum += gradesRecord[i];
+						break;
+					}
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid grade, try again...");
+				}
+			}
+		}
+		
+		System.out.printf("The average is %.1f", gradeSum / (double) numOfStud);
+		in.close();
+	}
+	
+	public void hex2Bin() throws NumberFormatException{
+		Scanner in = new Scanner(System.in);
+		String hexNum;
+		String[] hexBits;
+		Formatter f = new Formatter();
+		
+		System.out.print("Enter a Hexadecimal string: ");
+		hexNum = in.next();
+		hexBits = new String[hexNum.length()];
+		
+		System.out.printf("The equivalent binary for hexadecimal \"%s\" is ", hexNum);
+		
+		for (int i = 0; i < hexNum.length(); i++) {
+			int temp = Integer.parseInt(Character.toString(hexNum.charAt(i)), 16);
+			hexBits[i] = Integer.toBinaryString(temp);
+			System.out.printf("%04d ", temp);
+		}
+		
+		in.close();
+	}
+
+	public static void main(String[] args) {
+		ArrayExercise test = new ArrayExercise();
+		test.hex2Bin();
+
+	}
+
+}
