87c0fcbee80a2a12956a3179d2ff8346d2046105
Mark Wang <wangyouan0629@hotmail.com>
2015-10-28 08:58:43 +0800

    add some new features

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,153 @@
 import java.util.ArrayList;
 
 public class Main {
+	private static int rows, cols;
+	private static int[] allElements;
+	private static int[] newAllElements;
+	private static int[] dAllElements;
 	
-	public static ArrayList<int []> findPossibleNextPoint(int[] start_point, 
-			int[] allElement, int[][] grid) {
-		ArrayList<int []> possibleNextPoint = new ArrayList<>();
-		int i = start_point[0];
-		int j = start_point[1];
-		for (int m = j - 1; m < j + 2; m ++){
-			if (m == j) {
-				if (i + 1 < grid.length && grid[i + 1][j] >= 0) {
-					possibleNextPoint.add(new int [] {i + 1, j});
-				}
-			}else{
-				int possibleI = i;
-				int possibleJ = m;
-				if (m < 0) {
-					possibleJ = grid[0].length + m;
-				} else if (m == grid[0].length) {
-					possibleJ = 0;
-				}
-				if (allElement[possibleI * grid[0].length + possibleJ] >= 0) {
-					possibleNextPoint.add(new int[] {possibleI, possibleJ});
-				}
+	public static ArrayList<Integer> findPossibleNextPoint(int start_point) {
+		ArrayList<Integer> possibleNextPoint = new ArrayList<Integer>();
+		int[] m = new int [] {start_point -1, start_point + 1, 
+				start_point + cols};
+		int row = start_point / cols;
+		for (int i: m){
+			int testPoint = i;
+			if (i < row * cols) {
+				testPoint = start_point + cols - 1;
+			} else if (i == (row + 1) * cols) {
+				testPoint = row * cols;
+			} else if (testPoint > allElements.length) {
+				continue;
+			}
+			int[] temp = newAllElements.clone();
+			if (newAllElements[testPoint] >= 0) {
+				possibleNextPoint.add(testPoint);
 			}
 		}
 		return possibleNextPoint;
 	}
 	
-	public static int findEachMaxPath(int[] start_point, int[][] grid, 
-			int tempPath, int[] allElements) {
+	public static int findEachMaxPath(int start_point, int tempPath) {
 		int maxPath = -1;
-		int m = grid.length;
-		int n = grid[0].length;
 		
-		int[] newAllElements = new int[m * n];
-		for (int i = 0; i < n * m; i++) newAllElements[i] = allElements[i];
-		newAllElements[start_point[0] * n + start_point[1]] = -1;
+		newAllElements[start_point] = -1;
 		
-		ArrayList<int[]> possibleNextPoint = 
-				findPossibleNextPoint(start_point, newAllElements, grid);
+		ArrayList<Integer> possibleNextPoint = 
+				findPossibleNextPoint(start_point);
 		
 		if (possibleNextPoint.size() == 0) {
-			if (start_point[0] < grid.length - 1) maxPath = -1;
+			if (start_point / cols < rows - 1) maxPath = -1;
 			else maxPath = tempPath;
+			System.out.println();
 		} else {
-			for (int[] i : possibleNextPoint) {
+			for (int i : possibleNextPoint) {
 				int tempMaxPath = 0;
-				tempMaxPath = findEachMaxPath(i, grid, 
-						tempPath + grid[i[0]][i[1]], newAllElements);
+				System.out.print(allElements[i] + " ");
+				tempMaxPath = findEachMaxPath(i, 
+						tempPath + allElements[i]);
 				maxPath = Math.max(tempMaxPath, maxPath);
 			}
 		}
+		newAllElements[start_point] = allElements[start_point];
+		
+		int[] temp = newAllElements.clone();
+		int[] temp2 = allElements.clone();
 		
 		return maxPath;
 	}
 	
-	public static int findMaxPath(int[][] grid){
+	public static int findMaxPath(){
 		int maxPath = -1;
-		int[] allElements = new int[grid.length * grid[0].length];
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[0].length; j++){
-				allElements[i*grid.length + j] = grid[i][j];
-			}
-		}
 		
-		for (int i = 0; i < grid[0].length; i++) {
-			if (grid[0][i] < 0) {
+		for (int i = 0; i < cols; i++) {
+			newAllElements = allElements.clone();
+			if (allElements[i] < 0) {
 				continue;
 			}
-			int tempPath = findEachMaxPath(new int[] {0, i}, grid, grid[0][i],
-					allElements);
+			int tempPath = findEachMaxPath(i, allElements[i]);
+			maxPath = Math.max(tempPath, maxPath);
+		}
+		return maxPath;
+	}
+	
+	public static int max(int[] a) {
+		int maxNum = -9999999;
+		for (int i : a) {
+			maxNum = Math.max(maxNum, i);
+		}
+		return maxNum;
+	}
+	
+	public static int calculateThePathFromStart(int point) {
+		int path = 0;
+		int row = point / cols;
+		int col = point % cols;
+		int[] dFrom = new int[3];
+		if (allElements[point] < 0) {
+			path = -1;
+		} else {
+			if (col == 0) dFrom[0] = 0;
+			else dFrom[0] = dAllElements[point - 1];
+			
+			if (col == cols - 1) dFrom[1] = 0;
+			else dFrom[0] = dAllElements[point + 1];
+			
+			if (row == 0) dFrom[2] = -1;
+			else dFrom[0] = dAllElements[point - cols];
+			path = max(dFrom) + allElements[point];
+		}
+		return path;
+	}
+	
+	public static int findMaxPathDp(int start_point) {
+		int maxPath = -1;
+		
+		// find the distance before start point.
+		for (int i = start_point - 1; i >= 0; i--) 
+			dAllElements[i] = calculateThePathFromStart(i);
+		for (int i = start_point + 1; i < allElements.length; i++) 
+			dAllElements[i] = calculateThePathFromStart(i);
+		
+		for (int i = 0; i < cols; i++)
+			maxPath = Math.max(maxPath, dAllElements[rows * cols - i - 1]);
+		return maxPath;
+	}
+	
+	public static int findMaximumPath(){
+		int maxPath = -1;
+		
+		// find 
+		for (int i = 0; i < cols; i ++){
+			dAllElements = new int[rows * cols];
+			
+			// if start_point is 0, then the start point has nothing to do with
+			// our answer.
+			if (allElements[i] < 0) {
+				continue;
+			}
+			dAllElements[i] = allElements[i];
+			int tempPath = findMaxPathDp(i);
 			maxPath = Math.max(tempPath, maxPath);
 		}
 		return maxPath;
 	}
 
 	public static void main(String[] args) {
-		int n = Integer.parseInt(args[0]);
-		int m = Integer.parseInt(args[1]);
-		int[][] grid = new int[m][n];
+		cols = Integer.parseInt(args[0]);
+		rows = Integer.parseInt(args[1]);
+		allElements = new int[cols * rows];
 		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				grid[j][i] = Integer.parseInt(args[2 + i * m + j]);
-			}
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) allElements[i * cols + j] = 
+					Integer.parseInt(args[j * rows + i + 2]);
 		}
 		
-		System.out.println(findMaxPath(grid));
+		for (int i : allElements) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		System.out.println(findMaximumPath());
 	}
 }
