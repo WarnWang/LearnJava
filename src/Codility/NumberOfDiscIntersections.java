78526523ec690a37dbe17e9f413a3a8b46331770
WarnMark <wangyouan0629@hotmail.com>
2016-01-23 17:03:20 +0800

    fix some long int issue

--- src/Codility/NumberOfDiscIntersections.java
+++ src/Codility/NumberOfDiscIntersections.java
@@ -9,25 +9,25 @@ public class NumberOfDiscIntersections {
     public static void main(String[] args) {
         // put your codes here
         NumberOfDiscIntersections test = new NumberOfDiscIntersections();
-        System.out.print(test.solution(new int[]{1, 1, 1}));
+        System.out.print(test.solution(new int[]{1, 2147483647, 0}));
     }
 
     public int solution(int[] A) {
         // write your code in Java SE 8
         int result = 0, j = 0;
-        int[] rangeUpper = new int[A.length];
+        long[] rangeUpper = new long[A.length];
         int[] rangeLower = new int[A.length];
 
         for (int i = 0; i < A.length; i++) {
             rangeLower[i] = i - A[i];
-            rangeUpper[i] = i + A[i];
+            rangeUpper[i] = (long) i + (long) A[i];
         }
 
         Arrays.sort(rangeLower);
         Arrays.sort(rangeUpper);
 
         for (int i = 0; i < A.length - 1; i++) {
-            while (j < A.length && rangeUpper[i] >= rangeLower[j]) {
+            while (j < A.length && rangeUpper[i] >= (long) rangeLower[j]) {
                 j++;
             }
 
