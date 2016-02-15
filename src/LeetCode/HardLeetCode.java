25a0fde1f1c01cf2dff56b8ba1c43c9506ce928d
WarnMark <wangyouan0629@hotmail.com>
2016-02-15 14:41:11 +0800

    Solve the problem of maximum rectangle

--- src/LeetCode/HardLeetCode.java
+++ src/LeetCode/HardLeetCode.java
@@ -1,7 +1,5 @@
 package LeetCode;
 
-import java.util.Arrays;
-
 /**
  * Created by warn on 14/2/2016.
  */
@@ -9,7 +7,8 @@ public class HardLeetCode {
     public static void main(String[] args) {
         // put your codes here
         HardLeetCode test = new HardLeetCode();
-        System.out.println(test.candy(new int[]{1}));
+//        System.out.println(test.candy(new int[]{1}));
+//        System.out.println(test.maximalRectangle(new String[] {"000", "000", "000"}));
     }
 
     public int candy(int[] ratings) {
@@ -25,5 +24,53 @@ public class HardLeetCode {
         int total = 0;
         for (int i : childCandy) total += i;
         return total + ratings.length;
+    }
+
+
+    public int expandValue(char[][] matrix, int x, int y) {
+        int offsetX = 0, offsetY = 0;
+        int area;
+        for (int i = x + 1; i < matrix.length; i++) {
+            if (matrix[i][y] == '0') break;
+            offsetX++;
+        }
+
+        for (int i = y + 1; i < matrix[0].length; i++) {
+            if (matrix[x][i] == '0') break;
+            offsetY++;
+        }
+
+        area = offsetX + 1;
+        for (int i = 1; i <= offsetY; i++) {
+            int areaCount = 1;
+            for (int j = 1; j <= offsetX; j++) {
+                if (matrix[x + j][i + y] == '0') {
+                    offsetX = j - 1;
+                    break;
+                }
+                areaCount++;
+            }
+            int tempArea = areaCount * (i + 1);
+            if (tempArea > area) area = tempArea;
+        }
+        return area;
+    }
+
+
+    public int maximalRectangle(char[][] matrix) {
+        int height = matrix.length;
+        if (height == 0) return 0;
+        int width = matrix[0].length;
+        int result = 0;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                char temp = matrix[i][j];
+                if (temp == '1') {
+                    int tempResult = expandValue(matrix, i, j);
+                    if (tempResult > result) result = tempResult;
+                }
+            }
+        }
+        return result;
     }
 }
