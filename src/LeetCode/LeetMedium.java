92bf5399ee6c70bec72c7bbfb689f0802fb9913c
WarnMark <wangyouan0629@hotmail.com>
2016-02-16 14:59:09 +0800

    Solve Course Schedule

--- src/LeetCode/LeetMedium.java
+++ src/LeetCode/LeetMedium.java
@@ -1,9 +1,6 @@
 package LeetCode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by warn on 16/2/2016.
@@ -15,9 +12,11 @@ public class LeetMedium {
     public static void main(String[] args) {
         // put your codes here
         LeetMedium test = new LeetMedium();
-//        int[][] courseInfo = {{1, 0}, {0, 3}, {0, 2}, {3, 2}, {3, 1}};
-        int[][] courseInfo = {{1, 0}, {0, 1}};
-        System.out.println(test.canFinish(4, courseInfo));
+//        int[][] courseInfo = {{1, 0}, {0, 3}, {0, 2}, {3, 2}};
+        int[][] courseInfo = {{1, 0}};
+//        int[][] courseInfo = {{1, 0}, {0, 1}};
+//        int[][] courseInfo = {{0, 1}, {3, 1}, {1, 2}, {3, 2}, {0, 2}};
+        System.out.println(test.canFinishBFS(4, courseInfo));
     }
 
     private void getAllPrerequisiteCourse(Integer key) {
@@ -43,6 +42,108 @@ public class LeetMedium {
             newPrerequisite.addAll(prerequisite);
             prerequisiteMap.put(i[1], newPrerequisite);
         }
+        return true;
+    }
+
+    public boolean canFinishDFS(int numCourses, int[][] prerequisites) {
+        Map<Integer, Set<Integer>> prerequisiteMap = new HashMap<>();
+        for (int[] i : prerequisites) {
+            if (!prerequisiteMap.containsKey(i[0])) prerequisiteMap.put(i[0], new HashSet<>());
+            Set<Integer> prerequisite = prerequisiteMap.get(i[0]);
+            prerequisite.add(i[1]);
+            Stack<Integer> valueToExpand = new Stack<>();
+            Set<Integer> expandedValue = new HashSet<>();
+            valueToExpand.addAll(prerequisite);
+            while (!valueToExpand.isEmpty()) {
+                int course = valueToExpand.pop();
+                if (expandedValue.contains(course)) continue;
+                else expandedValue.add(course);
+                if (prerequisiteMap.containsKey(course)) {
+                    valueToExpand.addAll(prerequisiteMap.get(course));
+                    prerequisite.addAll(prerequisiteMap.get(course));
+                }
+                if (valueToExpand.contains(i[0])) return false;
+            }
+            if (expandedValue.contains(i[0])) return false;
+        }
+        prerequisiteMap.forEach((k, v) -> System.out.println("key_" + k + ":" + v.toString()));
+        return true;
+    }
+
+    public boolean canFinishBFS(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0 || prerequisites.length == 0) return true;
+        int[] courseCounter = new int[numCourses];
+        for (int[] i : prerequisites) courseCounter[i[0]]++;
+
+        LinkedList<Integer> courseInfo = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) if (courseCounter[i] == 0) courseInfo.add(i);
+
+        int numPreCourses = courseInfo.size();
+        while (!courseInfo.isEmpty()) {
+            int top = courseInfo.remove();
+            for (int[] i : prerequisites) {
+                if (i[1] == top) {
+                    courseCounter[i[0]]--;
+                    if (courseCounter[i[0]] == 0) {
+                        courseInfo.add(i[0]);
+                        numPreCourses++;
+                    }
+                }
+            }
+        }
+        return numCourses == numPreCourses;
+    }
+
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null) {
+            throw new IllegalArgumentException("illegal prerequisites array");
+        }
+
+        int len = prerequisites.length;
+
+        if (numCourses == 0 || len == 0) {
+            return true;
+        }
+
+        //track visited courses
+        int[] visit = new int[numCourses];
+
+        // use the map to store what courses depend on a course
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        for (int[] a : prerequisites) {
+            if (map.containsKey(a[1])) {
+                map.get(a[1]).add(a[0]);
+            } else {
+                ArrayList<Integer> l = new ArrayList<Integer>();
+                l.add(a[0]);
+                map.put(a[1], l);
+            }
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!canFinishDFS(map, visit, i))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean canFinishDFS(HashMap<Integer, ArrayList<Integer>> map, int[] visit, int i) {
+        if (visit[i] == -1)
+            return false;
+        if (visit[i] == 1)
+            return true;
+
+        visit[i] = -1;
+        if (map.containsKey(i)) {
+            for (int j : map.get(i)) {
+                if (!canFinishDFS(map, visit, j))
+                    return false;
+            }
+        }
+
+        visit[i] = 1;
+
         return true;
     }
 }
