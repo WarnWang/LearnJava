0ff6bea710683474587f75b367467bf452790479
WarnMark <wangyouan0629@hotmail.com>
2016-02-25 17:18:36 +0800

    Solve self crossing problem

--- src/LeetCode/LeetCodeEasyPuzzle.java
+++ src/LeetCode/LeetCodeEasyPuzzle.java
@@ -240,4 +240,32 @@ public class LeetCodeEasyPuzzle {
             if (guessArray[i] != 0 && secretArray[i] != 0) cows += Integer.min(guessArray[i], secretArray[i]);
         return bulls + "A" + cows + "B";
     }
+
+    public boolean isSelfCrossing(int[] x) {
+        for (int i = 3; i < x.length; i++) {
+            if (x[i - 1] <= x[i - 3] && x[i] >= x[i - 2]) return true;
+            if (i > 3 && x[i - 1] == x[i - 3] && (x[i - 2] - x[i] <= x[i - 4])) return true;
+            if (i > 4 && (x[i] + x[i - 4] >= x[i - 2]) && (x[i - 2] >= x[i - 4]) && (x[i - 3] <= x[i - 5] + x[i - 1])
+                    && (x[i - 1] <= x[i - 3])) return true;
+        }
+        return false;
+    /*               i-2
+    case 1 : i-1┌─┐
+                └─┼─>i
+                 i-3
+
+                    i-2
+    case 2 : i-1 ┌────┐
+                 └─══>┘i-3
+                 i  i-4      (i overlapped i-4)
+
+    case 3 :    i-4
+               ┌──┐
+               │i<┼─┐
+            i-3│ i-5│i-1
+               └────┘
+                i-2
+
+    */
+    }
 }
