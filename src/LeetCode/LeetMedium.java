be5764fd8086f1b4ca391956e61c8c13317fa094
WarnMark <wangyouan0629@hotmail.com>
2016-03-01 16:46:22 +0800

    Add one O(1) solution

--- src/LeetCode/LeetMedium.java
+++ src/LeetCode/LeetMedium.java
@@ -271,4 +271,48 @@ public class LeetMedium {
         count += countDigitOne(n - n / div * div);
         return count;
     }
+
+    public void setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length < 1) return;
+        Set<Integer> zeroRows = new HashSet<>();
+        Set<Integer> zeroColumns = new HashSet<>();
+        for (int i = 0, n = matrix.length, m = matrix[i].length; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroColumns.add(j);
+                    zeroRows.add(i);
+                }
+            }
+        }
+
+        for (int[] i : matrix) System.out.println(Arrays.toString(i));
+
+        for (int row : zeroRows) Arrays.fill(matrix[row], 0);
+
+        for (int col : zeroColumns) for (int i = 0; i < matrix.length; i++) matrix[i][col] = 0;
+    }
+
+    public void setZeroesO1(int[][] matrix) {
+        if (matrix == null || matrix.length < 1) return;
+        boolean row0 = false, col0 = false;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) row0 = true;
+                    if (j == 0) col0 = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if ((i == 0 && row0) || i != 0 && matrix[i][0] == 0 || j == 0 && col0 || j != 0 && matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+    }
 }
