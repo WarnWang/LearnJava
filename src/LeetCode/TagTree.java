3e45580ed4b097eeb41b1bb562b3e13ab854ac2a
Mark Wang <wangyouan0629@hotmail.com>
2016-04-02 09:31:17 +0800

    Use DFS to solve has path puzzles

--- src/LeetCode/TagTree.java
+++ src/LeetCode/TagTree.java
@@ -2,6 +2,7 @@ package LeetCode;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * Created by warn on 2/4/2016.
@@ -38,5 +39,38 @@ public class TagTree {
             }
         }
         return depth;
+    }
+
+    /**
+     * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values
+     * along the path equals the given sum.
+     * https://leetcode.com/problems/path-sum/
+     *
+     * @param root a binary tree
+     * @param sum  the target sum value
+     * @return whether this tree contains such sum or not
+     */
+    public boolean hasPathSumDFS(TreeNode root, int sum) {
+        if (root == null) return false;
+        Stack<TreeNode> nodeStack = new Stack<>();
+        Stack<Integer> sumStack = new Stack<>();
+        nodeStack.add(root);
+        sumStack.add(root.val);
+        while (!nodeStack.isEmpty()) {
+            TreeNode frontier = nodeStack.pop();
+            int temp = sumStack.pop();
+            if (frontier.left == null && frontier.right == null && temp == sum) return true;
+            else if (temp != sum) {
+                if (frontier.left != null) {
+                    nodeStack.push(frontier.left);
+                    sumStack.push(temp + frontier.left.val);
+                }
+                if (frontier.right != null) {
+                    nodeStack.push(frontier.right);
+                    sumStack.push(temp + frontier.right.val);
+                }
+            }
+        }
+        return false;
     }
 }
