367687853c068d43b31e2e6fbb7b699eb12e9dd8
Mark Wang <wangyouan0629@hotmail.com>
2016-04-03 21:23:23 +0800

    Improve performance, from 3ms to 2ms

--- src/LeetCode/TagTree.java
+++ src/LeetCode/TagTree.java
@@ -11,6 +11,16 @@ import java.util.Stack;
 public class TagTree {
 
     /**
+     * Given a binary tree, find the maximum path sum.
+     * For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree
+     * along the parent-child connections. The path does not need to go through the root.
+     *
+     * @param root a binary tree
+     * @return the maximum sum of path
+     */
+    private int maxValue = Integer.MIN_VALUE;
+
+    /**
      * Given a binary tree, find its minimum depth.
      * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf
      * node.
@@ -74,45 +84,37 @@ public class TagTree {
         return false;
     }
 
-    /**
-     * Given a binary tree, find the maximum path sum.
-     * For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree
-     * along the parent-child connections. The path does not need to go through the root.
-     *
-     * @param root a binary tree
-     * @return the maximum sum of path
-     */
-
     public int maxPathSum(TreeNode root) {
-        return maxPathSumAndBranch(root)[0];
+        maxPathSumAndBranch(root);
+        return maxValue;
     }
 
-    private int[] maxPathSumAndBranch(TreeNode root) {
-        if (root == null) return new int[]{0, 0};
+    private int maxPathSumAndBranch(TreeNode root) {
+        if (root == null) return 0;
         else if (root.left == null && root.right == null) {
-            return new int[]{root.val, root.val};
+            maxValue = Integer.max(root.val, maxValue);
+            return root.val;
         } else {
-            int[] leftBranch;
-            int[] rightBranch;
+            int leftBranch;
+            int rightBranch;
             if (root.left != null) leftBranch = maxPathSumAndBranch(root.left);
             else {
                 rightBranch = maxPathSumAndBranch(root.right);
-                int maxBranch = root.val + Integer.max(rightBranch[1], 0);
-                int maxSub = Integer.max(rightBranch[0], maxBranch);
-                return new int[]{maxSub, maxBranch};
+                int maxBranch = root.val + Integer.max(rightBranch, 0);
+                maxValue = Integer.max(maxValue, maxBranch);
+                return maxBranch;
             }
             if (root.right != null) rightBranch = maxPathSumAndBranch(root.right);
             else {
-                int maxBranch = Integer.max(leftBranch[1], 0) + root.val;
-                int maxSub = Integer.max(leftBranch[0], maxBranch);
-                return new int[]{maxSub, maxBranch};
+                int maxBranch = Integer.max(leftBranch, 0) + root.val;
+                maxValue = Integer.max(maxValue, maxBranch);
+                return maxBranch;
             }
-            int maxBranch = Integer.max(leftBranch[1], rightBranch[1]);
+            int maxBranch = Integer.max(leftBranch, rightBranch);
             maxBranch = Integer.max(maxBranch, 0) + root.val;
-            int maxSub = Integer.max(leftBranch[0], rightBranch[0]);
-            int currentSub = Integer.max(leftBranch[1], 0) + Integer.max(rightBranch[1], 0) + root.val;
-            maxSub = Integer.max(maxSub, currentSub);
-            return new int[]{maxSub, maxBranch};
+            int currentSub = Integer.max(leftBranch, 0) + Integer.max(rightBranch, 0) + root.val;
+            maxValue = Integer.max(maxValue, currentSub);
+            return maxBranch;
         }
     }
 }
