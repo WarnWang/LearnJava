e899a9de74314c4cbbac89d03b6fe6f026f36c67
Mark Wang <wangyouan0629@hotmail.com>
2016-04-11 17:04:45 +0800

    Finish one easy puzzles

--- src/LeetCode/TagString.java
+++ src/LeetCode/TagString.java
@@ -83,4 +83,55 @@ public class TagString {
         }
         return true;
     }
+
+    /**
+     * Implement strStr().
+     * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+     *
+     * @param haystack a string
+     * @param needle target string
+     * @return the index of first occurrence of needle in haystack
+     */
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null) return -1;
+        int lenHaystack = haystack.length();
+        int lenNeedle = needle.length();
+        if (lenNeedle == 0) return 0;
+        if (lenHaystack == 0 || lenHaystack < lenNeedle) return -1;
+        HashMap<Character, Integer> lastOccurrence = new HashMap<>();
+        char[] haystackCharArray = haystack.toCharArray();
+        char[] needleCharArray = needle.toCharArray();
+        for (char c: haystackCharArray) lastOccurrence.put(c, -1);
+        for (int i = 0; i < lenNeedle; i++) {
+            if (!lastOccurrence.containsKey(needleCharArray[i])) return -1;
+            lastOccurrence.put(needleCharArray[i] , i);
+        }
+
+//        for (int i = 0; i <= lenHaystack - lenNeedle; i++) {
+//            for (int j = lenNeedle - 1; j >= 0; j--) {
+//                if (haystackCharArray[i + j] == needleCharArray[j]){
+//                    if (j == 0) return i;
+//                } else {
+//                    int lastIndex = lastOccurrence.get(haystackCharArray[i + j]);
+//                    if (lastIndex == -1) i += j;
+//                    else if (lastIndex > j) i += lenNeedle - j - 2;
+//                    else i += lenNeedle - lastIndex - 3;
+//                    break;
+//                }
+//            }
+//        }
+        int i = lenNeedle-1;                                     // an index into the text
+        int k = lenNeedle-1;                                     // an index into the pattern
+        while (i < lenHaystack) {
+            if (haystackCharArray[i] == needleCharArray[k]) {                   // a matching character
+                if (k == 0) return i;                        // entire pattern has been found
+                i--;                                         // otherwise, examine previous
+                k--;                                         // characters of text/pattern
+            } else {
+                i += lenNeedle - Math.min(k, 1 + lastOccurrence.get(haystackCharArray[i])); // case analysis for jump step
+                k = lenNeedle - 1;                                   // restart at end of pattern
+            }
+        }
+        return -1;
+    }
 }
