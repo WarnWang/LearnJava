8fe38b3370a145c4157687bc0f54bdaf98aa0489
Mark Wang <wangyouan0629@hotmail.com>
2016-04-13 15:27:02 +0800

    Solve one Medium puzzle

--- src/LeetCode/TagLinkedList.java
+++ src/LeetCode/TagLinkedList.java
@@ -4,7 +4,7 @@ package LeetCode;
  * Created by warn on 22/3/2016.
  * Store problems with tag linked list
  */
-public class LinkedList {
+public class TagLinkedList {
     /**
      * Given a singly linked list, determine if it is a palindrome.
      *
@@ -89,6 +89,56 @@ public class LinkedList {
         }
         if (pointer == null) head = subTailHead;
         else pointer.next = subTailHead;
+        return head;
+    }
+
+    /**
+     * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than
+     * or equal to x.
+     * You should preserve the original relative order of the nodes in each of the two partitions.
+     *
+     * @param head a linked list
+     * @param x a value x
+     * @return nodes less than x come before nodes greater than or equal to x
+     */
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) return head;
+        ListNode i, tail;
+        if (head.val >= x) {
+            ListNode findLower = head;
+            while (findLower.next != null) {
+                if (findLower.next.val < x) {
+                    ListNode temp = findLower.next;
+                    findLower.next = findLower.next.next;
+                    temp.next = head;
+                    head = temp;
+                    break;
+                }
+                findLower = findLower.next;
+            }
+            if (findLower.next == null) return head;
+            else {
+                i = head.next;
+                tail = head;
+            }
+        } else {
+            ListNode j;
+            for (j = head; j.next != null && j.next.val < x; j = j.next);
+            if (j.next == null) return head;
+            else {
+                i = j.next;
+                tail = j;
+            }
+        }
+        for (ListNode frontier = tail; i.next !=null;) {
+            if (i.next.val < x) {
+                ListNode temp = i.next;
+                i.next = i.next.next;
+                temp.next = frontier.next;
+                frontier.next = temp;
+                frontier = frontier.next;
+            } else i = i.next;
+        }
         return head;
     }
 }
