7a4b03bbadb64f036f5251a454cd939aef3d560e
Mark Wang <wangyouan0629@hotmail.com>
2016-04-20 15:35:57 +0800

    Finish one middle puzzle

--- src/LeetCode/TagMath.java
+++ src/LeetCode/TagMath.java
@@ -241,4 +241,23 @@ public class TagMath {
 //        }
 //        return true;
     }
+
+    /**
+     * Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of
+     * those integers. Return the maximum product you can get.
+     * For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
+     * Note: you may assume that n is not less than 2.
+     *
+     * @param n: a positive integer
+     * @return Max product
+     */
+    public int integerBreak(int n) {
+        if (n < 2) return n;
+        int maxValue = 0, maxParts = Integer.max(n / 2, 2);
+        for (int nParts = 2; nParts <= maxParts; nParts++) {
+            int padding = n % nParts, part = n / nParts;
+            maxValue = Integer.max((int) (Math.pow(part + 1, padding) * Math.pow(part, nParts - padding)), maxValue);
+        }
+        return maxValue;
+    }
 }
