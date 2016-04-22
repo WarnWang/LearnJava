c346fb84197ea9ee2a366c35bce86e7a6b833354
Mark Wang <wangyouan0629@hotmail.com>
2016-04-22 09:16:50 +0800

    First version, but TLE

--- src/LeetCode/TagDFS.java
+++ src/LeetCode/TagDFS.java
@@ -235,4 +235,82 @@ public class TagDFS {
         }
     }
 
+    /**
+     * There are a total of n courses you have to take, labeled from 0 to n - 1.
+     * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is
+     * expressed as a pair: [0,1]
+     * Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should
+     * take to finish all courses.
+     * There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all
+     * courses, return an empty array.
+     *
+     * For example:
+     * 2, [[1,0]]
+     * There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct
+     * course order is [0,1]
+     * 4, [[1,0],[2,0],[3,1],[3,2]]
+     * There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both
+     * courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another
+     * correct ordering is[0,2,1,3].
+     *
+     * Note:
+     * The input prerequisites is a graph represented by a list of edges, not adjacency matrices.
+     *
+     * @param numCourses Course number
+     * @param prerequisites the prerequisite course of one course
+     * @return the course taken order
+     */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0) return new int[] {};
+        int[] courseSchedule = new int[numCourses];
+        if (prerequisites == null || prerequisites.length == 0) {
+            for (int i = 0; i < numCourses; i++) {
+                courseSchedule[i] = i;
+            }
+            return courseSchedule;
+        }
+
+        // key is the prerequisites course, and the target is
+        boolean[] isPrerequisite = new boolean[numCourses];
+        HashMap<Integer, ArrayList<Integer>> prerequisiteMap = new HashMap<>();
+        for (int[] prerequisite: prerequisites) {
+            isPrerequisite[prerequisite[0]] = true;
+            if (prerequisiteMap.containsKey(prerequisite[1])) prerequisiteMap.get(prerequisite[1]).add(prerequisite[0]);
+            else prerequisiteMap.put(prerequisite[1], new ArrayList<>(Collections.singletonList(prerequisite[0])));
+        }
+
+        if (prerequisiteMap.size() == numCourses) return courseSchedule;
+
+        int courseIndex = 0;
+        for (int i = 0; i < numCourses; i++) {
+            if (!prerequisiteMap.containsKey(i)) {
+                courseSchedule[courseIndex++] = i;
+                isPrerequisite[i] = false;
+            }
+        }
+
+        while (!prerequisiteMap.isEmpty()) {
+            ArrayList<Integer> courseTaken = new ArrayList<>();
+            for (int course: prerequisiteMap.keySet()) {
+                boolean canTake = true;
+                for (int prerequisite: prerequisiteMap.get(course)) {
+                    if (isPrerequisite[prerequisite]) {
+                        canTake = false;
+                        break;
+                    }
+                }
+                if (canTake) {
+                    courseSchedule[courseIndex++] = course;
+                    isPrerequisite[course] = false;
+                    courseTaken.add(course);
+                }
+            }
+            if (courseIndex == numCourses) break;
+            if (courseTaken.isEmpty()) return new int[] {};
+            else {
+                for (int course: courseTaken) prerequisiteMap.remove(course);
+            }
+        }
+        return courseSchedule;
+    }
 }
