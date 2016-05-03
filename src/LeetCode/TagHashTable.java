8f330d87ec1315e158061e75e85c5aa2ceaabc25
Mark Wang <wangyouan0629@hotmail.com>
2016-05-03 22:54:04 +0800

    Fix a small bug about this algorithm

--- src/LeetCode/TagHashTable.java
+++ src/LeetCode/TagHashTable.java
@@ -354,7 +354,7 @@ public class TagHashTable {
         PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(hashMap.size(), new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return o1[1] - o2[1];
+                return o2[1] - o1[1];
             }
         });
         for (int key :
