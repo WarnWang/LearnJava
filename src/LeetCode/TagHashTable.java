5c853306401ce790ecb90ebd3e40504db8dead19
Mark Wang <wangyouan0629@hotmail.com>
2016-05-13 09:57:17 +0800

    Sort version is slightly faster than hashtable version

--- src/LeetCode/TagHashTable.java
+++ src/LeetCode/TagHashTable.java
@@ -382,4 +382,13 @@ public class TagHashTable {
         }
         return false;
     }
+
+    public boolean containsDuplicateSort(int[] nums) {
+        if (nums == null || nums.length < 1) return false;
+        Arrays.sort(nums);
+        for (int i = 0, n =nums.length-1; i < n; i++) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
 }
