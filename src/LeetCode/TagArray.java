79f508113d5dc692f8debb1925602abf47866069
Mark Wang <wangyouan0629@hotmail.com>
2016-05-14 09:27:49 +0800

    Another O(n) solution

--- src/LeetCode/TagArray.java
+++ src/LeetCode/TagArray.java
@@ -792,4 +792,19 @@ public class TagArray {
         }
         return output;
     }
+
+    public int[] productExceptSelfWithoutDivision(int[] nums) {
+        int n = nums.length;
+        int output[] = new int[n];
+        Arrays.fill(output, 1);
+        for (int i = 0, left = 1; i < n; i++) {
+            output[i] *= left;
+            left *= nums[i];
+        }
+        for (int i = n - 1, right = 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
+    }
 }
