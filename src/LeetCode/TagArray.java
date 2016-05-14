f7a63a2dce819106d4fa59492844c4175132b8b1
Mark Wang <wangyouan0629@hotmail.com>
2016-05-14 09:18:33 +0800

    first solution, use division, O(n) time complexity, O(1) space complexity, 3ms

--- src/LeetCode/TagArray.java
+++ src/LeetCode/TagArray.java
@@ -760,4 +760,36 @@ public class TagArray {
         }
         return maximumProfit;
     }
+
+    /**
+     * Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the
+     * product of all the elements of nums except nums[i].
+     * <p>
+     * Solve it without division and in O(n).
+     * <p>
+     * For example, given [1,2,3,4], return [24,12,8,6].
+     *
+     * @param nums an array of n integers where n > 1
+     * @return an array output such that output[i] is equal to the product of all the elements of nums except nums[i]
+     */
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+        long numTimes = 1L;
+        int zeroIndex = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                if (zeroIndex != -1) return new int[nums.length];
+                else zeroIndex = i;
+            } else numTimes *= nums[i];
+        }
+        int[] output = new int[nums.length];
+        if (zeroIndex != -1) {
+            output[zeroIndex] = (int) numTimes;
+            return output;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = (int) (numTimes / nums[i]);
+        }
+        return output;
+    }
 }
