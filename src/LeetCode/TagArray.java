1b1674f68229feabfd79f43e2b5ac7d405f4246a
Mark Wang <wangyouan0629@hotmail.com>
2016-05-19 10:36:18 +0800

    finish a simple problem

--- src/LeetCode/TagArray.java
+++ src/LeetCode/TagArray.java
@@ -807,4 +807,47 @@ public class TagArray {
         }
         return output;
     }
+
+    /**
+     * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following
+     * properties:
+     * * Integers in each row are sorted from left to right.
+     * * The first integer of each row is greater than the last integer of the previous row.
+     * For example,
+     * <p>
+     * Consider the following matrix:
+     * <p>
+     * [
+     * [1,   3,  5,  7],
+     * [10, 11, 16, 20],
+     * [23, 30, 34, 50]
+     * ]
+     * Given target = 3, return true.
+     *
+     * @param matrix a 2-D matrix
+     * @param target target number
+     * @return whether target number in matrix or not
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return false;
+        return searchMatrix(matrix, target, 0, matrix.length * matrix[0].length);
+    }
+
+    private boolean searchMatrix(int[][] matrix, int target, int startIndex, int endIndex) {
+        if (startIndex >= endIndex) return false;
+        int n = matrix[0].length;
+        int startY = startIndex % n, startX = startIndex / n;
+        int endY = (endIndex - 1) % n, endX = (endIndex - 1) / n;
+        if (matrix[startX][startY] > target) return false;
+        else if (matrix[startX][startY] == target) return true;
+        else if (matrix[endX][endY] < target) return false;
+        else if (matrix[endX][endY] == target) return true;
+        else {
+            int midIndex = (startIndex + endIndex) / 2;
+            int midX = midIndex / n, midY = midIndex % n;
+            if (matrix[midX][midY] == target) return true;
+            else if (matrix[midX][midY] > target) return searchMatrix(matrix, target, startIndex, midIndex);
+            else return searchMatrix(matrix, target, midIndex + 1, endIndex);
+        }
+    }
 }
