5f07dd1dbf4fd062eaa0df111890720c58debccc
Mark Wang <wangyouan0629@hotmail.com>
2016-05-28 21:08:35 +0800

    Solve one design puzzles

---
+++ src/LeetCode/DesignPuzzles/NestedInteger.java
@@ -0,0 +1,19 @@
+package LeetCode.DesignPuzzles;
+
+import java.util.List;
+
+/**
+ * Created by warn on 28/5/2016.
+ */
+public interface NestedInteger {
+    //Returns true if this NestedInteger holds a single integer, rather than a nested list
+    public boolean isInteger();
+
+    //Returns the single integer that the NestedInteger holds, if it holds a single integer
+    //Returns null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    //Returns the nested list that this NestedInteger holds, if it holds a nested list
+    //Returns null if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+}
---
+++ src/LeetCode/DesignPuzzles/NestedIterator.java
@@ -0,0 +1,56 @@
+package LeetCode.DesignPuzzles;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Given a nested list of integers, implement an iterator to flatten it.
+ * Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+ * Example 1:
+ * Given the list [[1,1],2,[1,1]],
+ * By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+ * Example 2:
+ * Given the list [1,[4,[6]]],
+ * By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+ */
+public class NestedIterator implements Iterator<Integer> {
+    ArrayList<Integer> integers = new ArrayList<>();
+    int index = 0;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        for (NestedInteger nestedInteger: nestedList) {
+            if (nestedInteger.isInteger()) integers.add(nestedInteger.getInteger());
+            else addIntegerFromNestedList(nestedInteger);
+        }
+    }
+
+    private void addIntegerFromNestedList(NestedInteger integer) {
+        if (integer.isInteger()) {
+            integers.add(integer.getInteger());
+            return;
+        }
+        List<NestedInteger> nestedList = integer.getList();
+        for (NestedInteger nestedInteger: nestedList) {
+            if (nestedInteger.isInteger()) integers.add(nestedInteger.getInteger());
+            else addIntegerFromNestedList(nestedInteger);
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if (index < integers.size()) return integers.get(index++);
+        else return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < integers.size();
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
