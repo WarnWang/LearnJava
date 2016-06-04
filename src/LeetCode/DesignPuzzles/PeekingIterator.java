e50a598b471eba37b1d82301683f1a7edafdf8e0
Mark Wang <wangyouan0629@hotmail.com>
2016-06-04 11:07:00 +0800

    Change code to a better one

--- src/LeetCode/DesignPuzzles/PeekingIterator.java
+++ src/LeetCode/DesignPuzzles/PeekingIterator.java
@@ -1,7 +1,7 @@
 package LeetCode.DesignPuzzles;
 
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by warn on 4/6/2016.
@@ -20,38 +20,32 @@ import java.util.Iterator;
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 class PeekingIterator implements Iterator<Integer> {
-    private int cache;
-    private boolean cached;
+    private Integer cache;
     Iterator<Integer> iterator;
 
     public PeekingIterator(Iterator<Integer> iterator) {
         // initialize any member here.
-        cached = false;
         this.iterator = iterator;
+        cache = this.iterator.hasNext()? this.iterator.next(): null;
     }
 
     // Returns the next element in the iteration without advancing the iterator.
     public Integer peek() {
-        if (cached) return cache;
-        else {
-            cached = true;
-            cache = iterator.next();
-            return cache;
-        }
+        return cache;
     }
 
     // hasNext() and next() should behave the same as in the Iterator interface.
     // Override them if needed.
     @Override
     public Integer next() {
-        if (cached) {
-            cached = false;
-            return cache;
-        } else return iterator.next();
+        if (cache == null) throw new NoSuchElementException();
+        Integer nextInt = cache;
+        cache = iterator.hasNext() ? iterator.next() : null;
+        return nextInt;
     }
 
     @Override
     public boolean hasNext() {
-        return cached || iterator.hasNext();
+        return cache != null;
     }
 }
