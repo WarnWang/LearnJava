cd3ef1877ed7a4a2b397fb4b1754f3a7b01d84d8
Mark Wang <wangyouan0629@hotmail.com>
2016-06-17 10:52:20 +0800

    Solve an hard puzzles, I'm great

--- src/LeetCode/Algorithm/LinkedList/Solution.java
+++ src/LeetCode/Algorithm/LinkedList/Solution.java
@@ -11,8 +11,33 @@ import java.util.HashSet;
  */
 public class Solution {
 
+    public static void main(String args[]) {
+        int length = 10;
+        int[] linkedList = new int[length];
+        for (int i = 0; i < length; i++) {
+            linkedList[i] = i;
+        }
+//        HashMap<Integer, ListNode> listMap = new HashMap<>();
+        linkedList[9] = 2;
+        ListNode head = new ListNode(linkedList[0]);
+//        listMap.put(linkedList[0], head);
+        ListNode pointer = head;
+        for (int i = 1; i < length; i++) {
+//            if (listMap.containsKey(linkedList[i])) {
+//                pointer.next = listMap.get(linkedList[i]);
+//                break;
+//            } else
+            pointer.next = new ListNode(linkedList[i]);
+            pointer = pointer.next;
+        }
+
+        Solution test = new Solution();
+        System.out.println(test.reverseKGroup(head, 2).val);
+    }
+
     /**
      * Given a linked list, determine if it has a cycle in it.
+     *
      * @param head a linked list
      * @return contains cycle or not
      */
@@ -40,6 +65,7 @@ public class Solution {
 
     /**
      * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+     *
      * @param head a linked list
      * @return the start points of the cycle begins
      */
@@ -72,25 +98,62 @@ public class Solution {
         return null;
     }
 
-    public static void main(String args[]) {
-        int length = 10;
-        int[] linkedList = new int[length];
-        for (int i = 0; i < length; i++) {
-            linkedList[i] = i;
+    /**
+     * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+     * <p>
+     * If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+     * <p>
+     * You may not alter the values in the nodes, only nodes itself may be changed.
+     * <p>
+     * Only constant memory is allowed.
+     * <p>
+     * For example,
+     * Given this linked list: 1->2->3->4->5
+     * <p>
+     * For k = 2, you should return: 2->1->4->3->5
+     * <p>
+     * For k = 3, you should return: 3->2->1->4->5
+     *
+     * @param head a linked list
+     * @param k the group size
+     * @return reverse k group linked list
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k <= 1) return head;
+        if (head == null || head.next == null) return head;
+        int groupIndex = 0;
+        ListNode newList = null, newListTail = null, currentHead=head;
+        for (ListNode pointer = head; pointer != null; pointer = pointer.next) {
+            groupIndex++;
+            if (groupIndex == k) {
+                ListNode[] newTail = reverseGroup(currentHead, pointer);
+                if (newList == null) {
+                    newList = newTail[0];
+                    newListTail = newTail[1];
+                } else {
+                    newListTail.next = newTail[0];
+                    newListTail = newTail[1];
+                }
+                currentHead = pointer.next;
+                groupIndex = 0;
+            }
         }
-        HashMap<Integer, ListNode> listMap = new HashMap<>();
-        linkedList[9] = 2;
-        ListNode head = new ListNode(linkedList[0]);
-        listMap.put(linkedList[0], head);
-        ListNode pointer = head;
-        for (int i = 0; i < length; i++) {
-            if (listMap.containsKey(linkedList[i])) {
-                pointer.next = listMap.get(linkedList[i]);
-                break;
-            } else pointer.next = new ListNode(linkedList[i]);
+        if (groupIndex != 0) {
+            if (newList == null) return head;
+            else newListTail.next = currentHead;
         }
+        return newList;
+    }
 
-        Solution test = new Solution();
-        System.out.println(test.detectCycleHashSet(head).val);
+    private ListNode[] reverseGroup(ListNode head, ListNode tail) {
+        ListNode newHead = new ListNode(head.val), pointer = head.next, newTail = newHead;
+        for (; pointer != tail; pointer = pointer.next) {
+            ListNode temp = new ListNode(pointer.val);
+            temp.next = newHead;
+            newHead = temp;
+        }
+        ListNode temp = new ListNode(tail.val);
+        temp.next = newHead;
+        return new ListNode[] {newHead, newTail};
     }
 }
