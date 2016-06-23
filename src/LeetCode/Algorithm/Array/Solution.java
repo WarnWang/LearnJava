dd875816d3e7c75760e2c095b47532fd8e21c6f3
Mark Wang <wangyouan0629@hotmail.com>
2016-06-23 09:32:04 +0800

    171ms O(n^2) solution

--- src/LeetCode/Algorithm/Array/Solution.java
+++ src/LeetCode/Algorithm/Array/Solution.java
@@ -236,4 +236,26 @@ public class Solution {
             return hIndex(citations, middle + 1, end);
         }
     }
+
+    /**
+     * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+     * <p>
+     * Note:
+     * You must not modify the array (assume the array is read only).
+     * You must use only constant, O(1) extra space.
+     * Your runtime complexity should be less than O(n2).
+     * There is only one duplicate number in the array, but it could be repeated more than once.
+     *
+     * @param nums an array nums
+     * @return the duplicate number
+     */
+    public int findDuplicate(int[] nums) {
+        for (int i = 0, n = nums.length; i < n - 1; i++) {
+            int num = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                if (num == nums[j]) return num;
+            }
+        }
+        return 0;
+    }
 }
