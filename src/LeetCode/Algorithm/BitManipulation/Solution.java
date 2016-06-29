193b2f5ba9ca05fb837795f77ec7d1a6e227f882
Mark Wang <wangyouan0629@hotmail.com>
2016-06-29 10:31:13 +0800

    Solve one equation

--- src/LeetCode/Algorithm/BitManipulation/Solution.java
+++ src/LeetCode/Algorithm/BitManipulation/Solution.java
@@ -7,12 +7,35 @@ package LeetCode.Algorithm.BitManipulation;
 public class Solution {
     /**
      * Given an array of integers, every element appears twice except for one. Find that single one.
+     *
      * @param nums an array of integers
      * @return the only single number
      */
     public int singleNumber(int[] nums) {
         int operator = 0;
-        for (int num: nums) operator ^= num;
+        for (int num : nums) operator ^= num;
         return operator;
+    }
+
+    /**
+     * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range,
+     * inclusive.
+     * <p>
+     * For example, given the range [5, 7], you should return 4.
+     *
+     * @param m lower range
+     * @param n upper range
+     * @return the bit and value
+     */
+    public int rangeBitwiseAnd(int m, int n) {
+        if (m == 0) return 0;
+        if (m == n) return m;
+        int numberOfNum = n - m + 1;
+        int num = 0;
+        for (int i = 1; (m >> i) > 0; i++) {
+            if (numberOfNum < (1 << i) + 1 && ((m >> i) & 1) == 1 && ((n >> i) & 1) == 1)
+                num += (1 << i);
+        }
+        return num;
     }
 }
