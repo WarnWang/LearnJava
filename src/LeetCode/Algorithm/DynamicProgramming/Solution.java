f484a26ed3f366582b915fd7f7ac7d4acac2995b
Mark Wang <wangyouan0629@hotmail.com>
2016-07-03 14:08:52 +0800

    add one stop rule, to 5ms

--- src/LeetCode/Algorithm/DynamicProgramming/Solution.java
+++ src/LeetCode/Algorithm/DynamicProgramming/Solution.java
@@ -42,4 +42,56 @@ public class Solution {
         }
         return maxLength;
     }
+
+    /**
+     * ay you have an array for which the ith element is the price of a given stock on day i.
+     * <p>
+     * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one
+     * and sell one share of the stock multiple times) with the following restrictions:
+     * <p>
+     * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+     * After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+     * Example:
+     * <p>
+     * prices = [1, 2, 3, 0, 2]
+     * maxProfit = 3
+     * transactions = [buy, sell, cooldown, buy, sell]
+     *
+     * @param prices an array of stock price
+     * @return the max profit
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) return 0;
+        int n = prices.length;
+        int[] finalOperationMatrix = new int[n];
+        int[] profitList = new int[n];
+        int maxProfit = 0;
+        for (int i = 1; i < n; i++) {
+            int profit = 0;
+            for (int j = i - 1; j >= 0; j++) {
+                if (j == 0) {
+                    int currentProfit = prices[i] - prices[j];
+                    if (currentProfit > profit) {
+                        profit = currentProfit;
+                        finalOperationMatrix[i] = 1;
+                    }
+                } else if (prices[i] > prices[j]) {
+                    int currentProfit = prices[i] - prices[j];
+                    if (finalOperationMatrix[j - 1] == 0) currentProfit += profitList[j - 1];
+                    else if (j > 1) currentProfit += profitList[j - 2];
+                    if (currentProfit > profit) {
+                        profit = currentProfit;
+                        finalOperationMatrix[i] = 1;
+                    }
+                } else if (profitList[j] > profit) {
+                    profit = profitList[j];
+                    finalOperationMatrix[i] = 0;
+                } else if (prices[i] < prices[j]) break;
+
+            }
+            profitList[i] = profit;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
 }
