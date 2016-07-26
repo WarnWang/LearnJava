19df6f21879ca1d6f1bf2e403788f10928f4534a
Mark Wang <wangyouan0629@hotmail.com>
2016-07-26 11:18:17 +0800

    Solve one binary search problem

--- src/LeetCode/Algorithm/Search/BinarySearch.java
+++ src/LeetCode/Algorithm/Search/BinarySearch.java
@@ -32,4 +32,37 @@ public class BinarySearch {
         }
         return 0;
     }
+
+    /**
+     * Given a positive integer num, write a function which returns True if num is a perfect square else False.
+     * <p>
+     * Note: Do not use any built-in library function such as sqrt.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: 16
+     * Returns: True
+     * Example 2:
+     * <p>
+     * Input: 14
+     * Returns: False
+     *
+     * @param num a positive integer num
+     * @return whether this number is perfect square or not
+     */
+    public boolean isPerfectSquare(int num) {
+        if (num < 0) return false;
+        if (num <= 1) return true;
+        int start = 1;
+        int end = Math.min(num, 46340);
+        while (start < end) {
+            if (start * start == num || end * end == num) return true;
+            else if (start * start > num || end * end < num) return false;
+            int middle = start / 2 + end / 2;
+            if (middle * middle == num) return true;
+            else if (middle * middle > num) end = middle;
+            else start = middle;
+        }
+        return false;
+    }
 }
