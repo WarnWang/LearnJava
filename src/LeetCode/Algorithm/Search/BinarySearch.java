58fc59cc3de831f55a37d7d5a83ac5aada1788e6
Mark Wang <wangyouan0629@hotmail.com>
2016-07-26 11:05:32 +0800

    Solve one dp problem

--- src/LeetCode/Algorithm/Search/BinarySearch.java
+++ src/LeetCode/Algorithm/Search/BinarySearch.java
@@ -5,6 +5,14 @@ package LeetCode.Algorithm.Search;
  * Store solutions to question with tag binary search
  */
 public class BinarySearch {
+
+    public static void main(String[] args) {
+        BinarySearch test = new BinarySearch();
+        System.out.println(test.getMoneyAmount(11));
+        System.out.println(test.getMoneyAmount(14));
+        System.out.println(test.getMoneyAmount(8));
+    }
+
     int guess(int num) {
         return 0;
     }
@@ -27,4 +35,50 @@ public class BinarySearch {
         }
         return 0;
     }
+
+    /**
+     * We are playing the Guess Game. The game is as follows:
+     * <p>
+     * I pick a number from 1 to n. You have to guess which number I picked.
+     * <p>
+     * Every time you guess wrong, I'll tell you whether the number I picked is higher or lower.
+     * <p>
+     * However, when you guess a particular number x, and you guess wrong, you pay $x. You win the game when you guess
+     * the number I picked.
+     * <p>
+     * Example:
+     * <p>
+     * n = 10, I pick 8.
+     * <p>
+     * First round:  You guess 5, I tell you that it's higher. You pay $5.
+     * Second round: You guess 7, I tell you that it's higher. You pay $7.
+     * Third round:  You guess 9, I tell you that it's lower. You pay $9.
+     * <p>
+     * Game over. 8 is the number I picked.
+     * <p>
+     * You end up paying $5 + $7 + $9 = $21.
+     * Given a particular n ≥ 1, find out how much money you need to have to guarantee a win.
+     *
+     * @param n the maximum of guess number
+     * @return the money you should pay.
+     */
+    public int getMoneyAmount(int n) {
+        if (n <= 2) return n - 1;
+        moneyMatrix = new int[n + 1][n + 1];
+        return getSubMoney(1, n);
+    }
+
+    private int getSubMoney(int i, int j) {
+        if (i == j) return 0;
+        if (j - i == 1) moneyMatrix[i][j] = i;
+        if (moneyMatrix[i][j] > 0) return moneyMatrix[i][j];
+        int minMoney = Integer.MAX_VALUE;
+        for (int k = i + 1; k < j; k++) {
+            minMoney = Math.min(minMoney, k + Math.max(getSubMoney(i, k - 1), getSubMoney(k + 1, j)));
+        }
+        moneyMatrix[i][j] = minMoney;
+        return minMoney;
+    }
+
+    private int[][] moneyMatrix;
 }
