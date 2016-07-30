26fca6919235008aff4f851a266b8b3d32d20dce
Mark Wang <wangyouan0629@hotmail.com>
2016-07-30 09:16:14 +0800

    Solve one problem

--- src/LeetCode/Algorithm/DepthFirstSearch/BackTracking.java
+++ src/LeetCode/Algorithm/DepthFirstSearch/BackTracking.java
@@ -1,15 +1,15 @@
 package LeetCode.Algorithm.DepthFirstSearch;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by warn on 13/6/2016.
  * Store solution to test with backtracking
  */
 public class BackTracking {
+    boolean findPath;
+    private HashSet<String> contains;
+
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> combinations = new ArrayList<>();
         if (k > n) return combinations;
@@ -63,7 +63,6 @@ public class BackTracking {
         }
         return result;
     }
-    private HashSet<String> contains;
 
     private void getSubSetsWithOutDup(int[] nums, int index, Integer[] path, int pathSize, List<List<Integer>> result) {
         for (int i = index; i < nums.length; i++) {
@@ -76,6 +75,60 @@ public class BackTracking {
                 result.add(Arrays.asList(subSet));
             }
             getSubSetsWithOutDup(nums, i + 1, path, pathSize + 1, result);
+        }
+    }
+
+    /**
+     * The gray code is a binary numeral system where two successive values differ in only one bit.
+     * <p>
+     * Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray
+     * code. A gray code sequence must begin with 0.
+     * <p>
+     * For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+     * <p>
+     * 00 - 0
+     * 01 - 1
+     * 11 - 3
+     * 10 - 2
+     * Note:
+     * For a given n, a gray code sequence is not uniquely defined.
+     * <p>
+     * For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+     *
+     * @param n the bit number
+     * @return an sequence of gray code
+     */
+    public List<Integer> grayCode(int n) {
+        if (n == 0) return Collections.singletonList(0);
+        else if (n < 0) return Collections.emptyList();
+        int totalNum = 1 << n;
+        Integer[] path = new Integer[totalNum];
+        boolean[] isUsed = new boolean[totalNum];
+        isUsed[0] = true;
+        path[0] = 0;
+        findPath = false;
+        findGrayCodeArray(path, isUsed, 1, n);
+        return Arrays.asList(path);
+    }
+
+    private void findGrayCodeArray(Integer[] path, boolean[] used, int index, int n) {
+        if (findPath) return;
+        else if (index == path.length) {
+            findPath = true;
+        } else {
+            int lastInteger = path[index - 1];
+            for (int i = 0; i < n; i++) {
+                if (findPath) break;
+                int bitOperation = 1 << i;
+                int newInteger = ((bitOperation & lastInteger) == bitOperation) ? (lastInteger - bitOperation) :
+                        (lastInteger + bitOperation);
+                if (!used[newInteger]) {
+                    path[index] = newInteger;
+                    used[newInteger] = true;
+                    findGrayCodeArray(path, used, index + 1, n);
+                    used[newInteger] = false;
+                }
+            }
         }
     }
 }
