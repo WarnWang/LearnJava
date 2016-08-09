9b29d8d238287a2607eb037466a42801a26617af
Mark Wang <wangyouan0629@hotmail.com>
2016-08-09 09:25:32 +0800

    Solve Pascal's Triangle problem

---
+++ src/LeetCode/Algorithm/Array/Solution2.java
@@ -0,0 +1,46 @@
+package LeetCode.Algorithm.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by warn on 9/8/2016.
+ * Store solution to Array puzzles
+ */
+public class Solution2 {
+    /**
+     * Given numRows, generate the first numRows of Pascal's triangle.
+     * <p>
+     * For example, given numRows = 5,
+     * Return
+     * <p>
+     * [
+     * [1],
+     * [1,1],
+     * [1,2,1],
+     * [1,3,3,1],
+     * [1,4,6,4,1]
+     * ]
+     *
+     * @param numRows the row number needed
+     * @return the result array list
+     */
+    public List<List<Integer>> generate(int numRows) {
+        ArrayList<List<Integer>> pascalTriangle = new ArrayList<>();
+        if (numRows <= 0) return pascalTriangle;
+        pascalTriangle.add(Collections.singletonList(1));
+        for (int i = 2; i <= numRows; i++) {
+            Integer[] row = new Integer[i];
+            List<Integer> lastRow = pascalTriangle.get(i - 2);
+            row[0] = 1;
+            for (int j = 1; j < i - 1; j++) {
+                row[j] = lastRow.get(j - 1) + lastRow.get(j);
+            }
+            row[i - 1] = 1;
+            pascalTriangle.add(Arrays.asList(row));
+        }
+        return pascalTriangle;
+    }
+}
