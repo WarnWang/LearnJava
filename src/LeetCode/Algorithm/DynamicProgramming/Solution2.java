40c82b75589486f0ee6d64ef68095c4c3a7fd1fc
Mark Wang <wangyouan0629@hotmail.com>
2016-08-15 11:31:34 +0800

    Solve one puzzle

--- src/LeetCode/Algorithm/DynamicProgramming/Solution2.java
+++ src/LeetCode/Algorithm/DynamicProgramming/Solution2.java
@@ -1,6 +1,5 @@
 package LeetCode.Algorithm.DynamicProgramming;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
 
@@ -119,5 +118,54 @@ public class Solution2 {
             }
         }
         return dp[k][n - 1];
+    }
+
+    /**
+     * The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. The dungeon
+     * consists of M x N rooms laid out in a 2D grid. Our valiant knight (K) was initially positioned in the top-left
+     * room and must fight his way through the dungeon to rescue the princess.
+     * <p>
+     * The knight has an initial health point represented by a positive integer. If at any point his health point drops
+     * to 0 or below, he dies immediately.
+     * <p>
+     * Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering these
+     * rooms; other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive
+     * integers).
+     * <p>
+     * In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward
+     * in each step.
+     * <p>
+     * <p>
+     * Write a function to determine the knight's minimum initial health so that he is able to rescue the princess.
+     * <p>
+     * For example, given the dungeon below, the initial health of the knight must be at least 7 if he follows the
+     * optimal path RIGHT-> RIGHT -> DOWN -> DOWN.
+     * <p>
+     * -2 (K)	-3	3
+     * -5	-10	1
+     * 10	30	-5 (P)
+     *
+     * @param dungeon a map
+     * @return the minimal health needed
+     */
+    public int calculateMinimumHP(int[][] dungeon) {
+        if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0) return 0;
+        int h = dungeon[0].length, w = dungeon.length;
+        int maxRow = w - 1, maxCol = h - 1;
+        int[][] minimalHealth = new int[w][h];
+        minimalHealth[maxRow][maxCol] = Math.max(1 - dungeon[maxRow][maxCol], 1);
+        for (int i = maxCol - 1; i >= 0; i--)
+            minimalHealth[maxRow][i] = Math.max(minimalHealth[maxRow][i + 1] - dungeon[maxRow][i], 1);
+        for (int i = maxRow - 1; i >= 0; i--)
+            minimalHealth[i][maxCol] = Math.max(minimalHealth[i + 1][maxCol] - dungeon[i][maxCol], 1);
+
+        for (int i = maxRow - 1; i >= 0; i--) {
+            for (int j = maxCol - 1; j >= 0; j--) {
+                minimalHealth[i][j] = Math.max(Math.min(minimalHealth[i + 1][j],
+                        minimalHealth[i][j + 1]) - dungeon[i][j], 1);
+            }
+        }
+
+        return Math.max(minimalHealth[0][0], 1);
     }
 }
