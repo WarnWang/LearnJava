7cd5bd9857323457d39d1ca837f3e834b788aaf2
Mark Wang <wangyouan0629@hotmail.com>
2016-09-02 10:19:34 +0800

    Solve one puzzle

--- src/LeetCode/Algorithm/String/Solution2.java
+++ src/LeetCode/Algorithm/String/Solution2.java
@@ -223,4 +223,57 @@ public class Solution2 {
         }
         return new String(result);
     }
+
+    /**
+     * Compare two version numbers version1 and version2.
+     * If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
+     * <p>
+     * You may assume that the version strings are non-empty and contain only digits and the . character.
+     * The . character does not represent a decimal point and is used to separate number sequences.
+     * For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision
+     * of the second first-level revision.
+     * <p>
+     * Here is an example of version numbers ordering:
+     * <p>
+     * 0.1 < 1.1 < 1.2 < 13.37
+     *
+     * @param version1 the first version number
+     * @param version2 the second version number
+     * @return which one is bigger
+     */
+    public int compareVersion(String version1, String version2) {
+        if (version1 == null && version2 == null) return 0;
+        else if (version1 == null) return -1;
+        else if (version2 == null) return 1;
+
+        int len1 = version1.length(), len2 = version2.length();
+        int i1 = 0, i2 = 0;
+        while (i1 < len1 || i2 < len2) {
+            int n1 = 0, n2 = 0;
+            for (int i = i1; i < len1; i++) {
+                if (version1.charAt(i) == '.') {
+                    n1 = Integer.parseInt(version1.substring(i1, i));
+                    i1 = i + 1;
+                    break;
+                } else if (i == len1 - 1) {
+                    n1 = Integer.parseInt(version1.substring(i1, len1));
+                    i1 = len1;
+                }
+            }
+
+            for (int i = i2; i < len2; i++) {
+                if (version2.charAt(i) == '.') {
+                    n2 = Integer.parseInt(version2.substring(i2, i));
+                    i2 = i + 1;
+                    break;
+                } else if (i == len2 - 1) {
+                    n2 = Integer.parseInt(version2.substring(i2, len2));
+                    i2 = len2;
+                }
+            }
+            if (n1 > n2) return 1;
+            else if (n2 > n1) return -1;
+        }
+        return 0;
+    }
 }
