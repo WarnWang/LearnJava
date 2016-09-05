97ea64569771099377607a9c78df6ff6a7ab39bf
Mark Wang <wangyouan0629@hotmail.com>
2016-09-05 09:31:27 +0800

    Solve one string problem

---
+++ src/LeetCode/Algorithm/DepthFirstSearch/Solution2.java
@@ -0,0 +1,10 @@
+package LeetCode.Algorithm.DepthFirstSearch;
+
+import java.util.ArrayList;
+
+/**
+ * Created by warn on 5/9/2016.
+ * Store solution to complete binary tree
+ */
+public class Solution2 {
+}
--- src/LeetCode/Algorithm/String/Solution2.java
+++ src/LeetCode/Algorithm/String/Solution2.java
@@ -276,4 +276,69 @@ public class Solution2 {
         }
         return 0;
     }
+
+    /**
+     * Suppose we abstract our file system by a string in the following manner:
+     * <p>
+     * The string "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext" represents:
+     * <p>
+     * dir
+     * subdir1
+     * subdir2
+     * file.ext
+     * The directory dir contains an empty sub-directory subdir1 and a sub-directory subdir2 containing a file file.ext.
+     * <p>
+     * The string "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext" represents:
+     * <p>
+     * dir
+     * subdir1
+     * file1.ext
+     * subsubdir1
+     * subdir2
+     * subsubdir2
+     * file2.ext
+     * The directory dir contains two sub-directories subdir1 and subdir2. subdir1 contains a file file1.ext and an
+     * empty second-level sub-directory subsubdir1. subdir2 contains a second-level sub-directory subsubdir2 containing
+     * a file file2.ext.
+     * <p>
+     * We are interested in finding the longest (number of characters) absolute path to a file within our file system.
+     * For example, in the second example above, the longest absolute path is "dir/subdir2/subsubdir2/file2.ext", and
+     * its length is 32 (not including the double quotes).
+     * <p>
+     * Given a string representing the file system in the above format, return the length of the longest absolute path
+     * to file in the abstracted file system. If there is no file in the system, return 0.
+     * <p>
+     * Note:
+     * The name of a file contains at least a . and an extension.
+     * The name of a directory or sub-directory will not contain a ..
+     *
+     * @param input a file system string
+     * @return the longest absolute path
+     */
+    public int lengthLongestPath(String input) {
+        if (input == null || input.length() == 0) return 0;
+        String[] pathList = input.split("\\n");
+        String[] path = new String[pathList.length];
+        int level;
+//        String longest = "";
+        int longestLength = 0;
+        for (String i : pathList) {
+            String removeUseLess = i.replaceAll("\\t", "");
+            level = i.length() - removeUseLess.length();
+            path[level] = removeUseLess;
+            if (removeUseLess.contains(".")) {
+                StringBuilder newString = new StringBuilder();
+                for (int j = 0; j < level; j++) {
+                    newString.append(path[j]);
+                    newString.append('/');
+                }
+                newString.append(removeUseLess);
+                if (newString.length() > longestLength) {
+                    longestLength = newString.length();
+//                    longest = newString.toString();
+                }
+            }
+        }
+        return longestLength;
+    }
 }
