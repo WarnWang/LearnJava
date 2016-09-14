3c0146ad58c27fc49d98df59e4d1cea758165063
Mark Wang <wangyouan0629@hotmail.com>
2016-09-14 10:57:51 +0800

    Add another solution

--- src/LeetCode/Algorithm/Sort/Solution.java
+++ src/LeetCode/Algorithm/Sort/Solution.java
@@ -1,6 +1,8 @@
 package LeetCode.Algorithm.Sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringJoiner;
 
 /**
  * Created by warn on 7/9/2016.
@@ -54,5 +56,30 @@ public class Solution {
             maximumGap = Math.max(maximumGap, Math.abs(nums[i] - nums[i - 1]));
         }
         return maximumGap;
+    }
+
+    /**
+     * Given a list of non negative integers, arrange them such that they form the largest number.
+     * <p>
+     * For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+     * <p>
+     * Note: The result may be very large, so you need to return a string instead of an integer.
+     *
+     * @param nums a list of non negative integers
+     * @return the largest integer
+     */
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0) return "0";
+        else if (nums.length == 1) return Integer.toString(nums[0]);
+        String[] numsString = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numsString[i] = Integer.toString(nums[i]);
+        }
+        Arrays.sort(numsString, ((o1, o2) -> (o1 + o2).compareTo(o2 + o1)));
+        String result = String.join("", (CharSequence[]) numsString);
+        for (int i = 0; i < result.length(); i++) {
+            if (result.charAt(i) != '0') return result.substring(i);
+        }
+        return "0";
     }
 }
