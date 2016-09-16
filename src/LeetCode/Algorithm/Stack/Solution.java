9bd7e60db1a64d79d3ee95e94f2b07dbabf02b26
Mark Wang <wangyouan0629@hotmail.com>
2016-09-16 11:08:17 +0800

    Solve one stack question

--- src/LeetCode/Algorithm/Stack/Solution.java
+++ src/LeetCode/Algorithm/Stack/Solution.java
@@ -1,8 +1,5 @@
 package LeetCode.Algorithm.Stack;
 
-import java.util.Arrays;
-import java.util.Stack;
-
 /**
  * Created by warn on 2/6/2016.
  * Store solution to tag stack
@@ -45,5 +42,63 @@ public class Solution {
             newPath.append(newDirList[i]);
         }
         return newPath.toString();
+    }
+
+    /**
+     * Given an encoded string, return it's decoded string.
+     * <p>
+     * The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated
+     * exactly k times. Note that k is guaranteed to be a positive integer.
+     * <p>
+     * You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed,
+     * etc.
+     * <p>
+     * Furthermore, you may assume that the original data does not contain any digits and that digits are only for
+     * those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+     * <p>
+     * Examples:
+     * <p>
+     * s = "3[a]2[bc]", return "aaabcbc".
+     * s = "3[a2[c]]", return "accaccacc".
+     * s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+     *
+     * @param s an encoded string
+     * @return it's decoded string
+     */
+    public String decodeString(String s) {
+        if (s == null || s.length() == 0) return "";
+        StringBuilder newString = new StringBuilder();
+        for (i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int start = s.indexOf('[', i);
+                int repeatTime = Integer.parseInt(s.substring(i, start));
+                newString.append(decodeString(s, repeatTime, start + 1));
+            } else {
+                newString.append(c);
+            }
+        }
+        return newString.toString();
+    }
+
+    private int i;
+
+    private String decodeString(String s, int repeatTimes, int startIndex) {
+
+        StringBuilder newString = new StringBuilder();
+        for (i = startIndex; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ']') break;
+            else if (Character.isDigit(c)) {
+                int start = s.indexOf('[', i);
+                int repeatTime = Integer.parseInt(s.substring(i, start));
+                newString.append(decodeString(s, repeatTime, start + 1));
+            } else newString.append(c);
+        }
+        StringBuilder result = new StringBuilder();
+        for (int j = 0; j < repeatTimes; j++) {
+            result.append(newString);
+        }
+        return result.toString();
     }
 }
