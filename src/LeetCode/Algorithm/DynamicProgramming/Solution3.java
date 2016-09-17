6433e3599767b7fb020d5aaaef67def1252a5923
Mark Wang <wangyouan0629@hotmail.com>
2016-09-17 22:27:01 +0800

    Solve one stack question

--- src/LeetCode/Algorithm/DynamicProgramming/Solution3.java
+++ src/LeetCode/Algorithm/DynamicProgramming/Solution3.java
@@ -147,4 +147,43 @@ public class Solution3 {
         }
         return result;
     }
+
+    /**
+     * Given a string s and a string t, check if s is subsequence of t.
+     * <p>
+     * You may assume that there is only lower case English letters in both s and t. t is potentially a very long
+     * (length ~= 500,000) string, and s is a short string (<=100).
+     * <p>
+     * A subsequence of a string is a new string which is formed from the original string by deleting some (can be none)
+     * of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a
+     * subsequence of "abcde" while "aec" is not).
+     * <p>
+     * Example 1:
+     * s = "abc", t = "ahbgdc"
+     * <p>
+     * Return true.
+     * <p>
+     * Example 2:
+     * s = "axc", t = "ahbgdc"
+     * <p>
+     * Return false.
+     * <p>
+     * Follow up:
+     * If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if
+     * T has its subsequence. In this scenario, how would you change your code?
+     *
+     * @param s one string
+     * @param t another string
+     * @return if s is subsequence of t or not
+     */
+    public boolean isSubsequence(String s, String t) {
+        if (s == null || s.length() == 0) return true;
+        if (t == null || t.length() == 0) return false;
+        char[] sArray = s.toCharArray(), tArray = t.toCharArray();
+        for (int i = 0, nS = sArray.length, nT = tArray.length, j = 0; i < nT; i++) {
+            if (tArray[i] == sArray[j]) j++;
+            if (j == nS) return true;
+        }
+        return false;
+    }
 }
