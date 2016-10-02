ec5ad0ad772798245eba484063a231e18eab82d6
Mark Wang <wangyouan0629@hotmail.com>
2016-10-02 19:52:59 +0800

    Solve one puzzle

--- src/LeetCode/Algorithm/DynamicProgramming/Solution3.java
+++ src/LeetCode/Algorithm/DynamicProgramming/Solution3.java
@@ -9,6 +9,8 @@ import java.util.List;
  * Store some DP puzzles solution
  */
 public class Solution3 {
+    private int[][] minimalDistanceInfo;
+
     /**
      * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed)
      * parentheses substring.
@@ -217,8 +219,6 @@ public class Solution3 {
         return minimalDistanceInfo[0][0];
     }
 
-    private int[][] minimalDistanceInfo;
-
     private int minDistance(String word1, String word2, int index1, int index2, int n1, int n2) {
 
         if (index1 == n1) return n2 - index2;
@@ -233,5 +233,53 @@ public class Solution3 {
             minimalDistanceInfo[index1][index2] = minimalDistance;
         }
         return minimalDistanceInfo[index1][index2];
+    }
+
+    /**
+     * A sequence of numbers is called a wiggle sequence if the differences between successive numbers strictly
+     * alternate between positive and negative. The first difference (if one exists) may be either positive or
+     * negative. A sequence with fewer than two elements is trivially a wiggle sequence.
+     * <p>
+     * For example, [1,7,4,9,2,5] is a wiggle sequence because the differences (6,-3,5,-7,3) are alternately positive
+     * and negative. In contrast, [1,4,7,2,5] and [1,7,4,5,5] are not wiggle sequences, the first because its first two
+     * differences are positive and the second because its last difference is zero.
+     * <p>
+     * Given a sequence of integers, return the length of the longest subsequence that is a wiggle sequence. A
+     * subsequence is obtained by deleting some number of elements (eventually, also zero) from the original sequence,
+     * leaving the remaining elements in their original order.
+     * <p>
+     * Examples:
+     * Input: [1,7,4,9,2,5]
+     * Output: 6
+     * The entire sequence is a wiggle sequence.
+     * <p>
+     * Input: [1,17,5,10,13,15,10,5,16,8]
+     * Output: 7
+     * There are several subsequences that achieve this length. One is [1,17,10,13,10,16,8].
+     * <p>
+     * Input: [1,2,3,4,5,6,7,8,9]
+     * Output: 2
+     *
+     * @param nums an array list
+     * @return the length of longest wiggle sub sequence
+     */
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int currentSign = 0;
+        int currentLength = 1;
+        int last = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int current = nums[i];
+            if (current != last) {
+                if ((current - last) * currentSign < 0) {
+                    currentSign = -currentSign;
+                    currentLength++;
+                } else if (currentSign == 0) {
+                    currentLength = 2;
+                    currentSign = (current - last) / Math.abs(current - last);
+                } last = current;
+            }
+        }
+        return currentLength;
     }
 }
