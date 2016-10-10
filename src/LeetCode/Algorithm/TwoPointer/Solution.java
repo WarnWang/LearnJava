1a0adf5126f3c45a338ae3f8388f2388a0d1ef12
Mark Wang <wangyouan0629@hotmail.com>
2016-10-10 09:49:50 +0800

    Solve one problem

--- src/LeetCode/Algorithm/TwoPointer/Solution.java
+++ src/LeetCode/Algorithm/TwoPointer/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode.Algorithm.TwoPointer;
 
+import LeetCode.DataTypes.ListNode;
+
 /**
  * Created by warn on 29/9/2016.
  * Store problem with tag two pointer
@@ -17,7 +19,7 @@ public class Solution {
      */
     public boolean isPalindrome(String s) {
         if (s == null || s.length() <= 1) return true;
-        for (int i = 0, j = s.length() - 1; i < j;) {
+        for (int i = 0, j = s.length() - 1; i < j; ) {
             char cI = s.charAt(i), cJ = s.charAt(j);
             while (!Character.isAlphabetic(cI) || !Character.isDigit(cI)) {
                 i++;
@@ -37,5 +39,39 @@ public class Solution {
             }
         }
         return true;
+    }
+
+    /**
+     * Given a linked list, remove the nth node from the end of list and return its head.
+     * <p>
+     * For example,
+     * <p>
+     * Given linked list: 1->2->3->4->5, and n = 2.
+     * <p>
+     * After removing the second node from the end, the linked list becomes 1->2->3->5.
+     * Note:
+     * Given n will always be valid.
+     * Try to do this in one pass.
+     *
+     * @param head a linked list
+     * @param n the node that need to be removed
+     * @return the removed linked list
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode node = head;
+        ListNode tail = head;
+        for (int i = 0; i < n; i++) {
+            tail = tail.next;
+        }
+        if (tail == null) return head.next;
+        else {
+            while (tail.next != null) {
+                tail = tail.next;
+                node = node.next;
+            }
+
+            node.next = node.next.next;
+            return head;
+        }
     }
 }
