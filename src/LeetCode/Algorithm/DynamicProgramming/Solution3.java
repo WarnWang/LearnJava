c6a1e41c171c3e30b4b1c0fc11110829e9d050a8
Mark Wang <wangyouan0629@hotmail.com>
2016-10-12 09:34:17 +0800

    Solve one problem

--- src/LeetCode/Algorithm/DynamicProgramming/Solution3.java
+++ src/LeetCode/Algorithm/DynamicProgramming/Solution3.java
@@ -277,9 +277,45 @@ public class Solution3 {
                 } else if (currentSign == 0) {
                     currentLength = 2;
                     currentSign = (current - last) / Math.abs(current - last);
-                } last = current;
+                }
+                last = current;
             }
         }
         return currentLength;
+    }
+
+    /**
+     * Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+     * <p>
+     * Note:
+     * Both the array size and each of the array element will not exceed 100.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [1, 5, 11, 5]
+     * <p>
+     * Output: true
+     * <p>
+     * Explanation: The array can be partitioned as [1, 5, 5] and [11].
+     * Example 2:
+     * <p>
+     * Input: [1, 2, 3, 5]
+     * <p>
+     * Output: false
+     * <p>
+     * Explanation: The array cannot be partitioned into equal sum subsets.
+     *
+     * @param nums an input array
+     * @return whether this array can be divided or not
+     */
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length < 2) return false;
+        int sum = 0;
+        for (int num : nums) sum += num;
+        return sum % 2 == 0 && canPartition(nums, sum / 2, nums.length - 1);
+    }
+
+    private boolean canPartition(int[] nums, int target, int index) {
+        return target == 0 || !(index < 0 || target < 0) && (canPartition(nums, target - nums[index], index - 1) || canPartition(nums, target, index - 1));
     }
 }
