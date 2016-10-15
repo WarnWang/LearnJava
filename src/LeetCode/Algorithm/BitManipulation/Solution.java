96bf8ed85e9b9a69672d1af31981946f808cb43b
Mark Wang <wangyouan0629@hotmail.com>
2016-10-15 20:26:24 +0800

    very good solution

--- src/LeetCode/Algorithm/BitManipulation/Solution.java
+++ src/LeetCode/Algorithm/BitManipulation/Solution.java
@@ -157,4 +157,53 @@ public class Solution {
             else result[0] ^= num;
         return result;
     }
+
+    /**
+     * Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, two’s complement method
+     * is used.
+     * <p>
+     * Note:
+     * <p>
+     * All letters in hexadecimal (a-f) must be in lowercase.
+     * The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single
+     * zero character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
+     * The given number is guaranteed to fit within the range of a 32-bit signed integer.
+     * You must not use any method provided by the library which converts/formats the number to hex directly.
+     * Example 1:
+     * <p>
+     * Input:
+     * 26
+     * <p>
+     * Output:
+     * "1a"
+     * Example 2:
+     * <p>
+     * Input:
+     * -1
+     * <p>
+     * Output:
+     * "ffffffff"
+     *
+     * @param num an integer
+     * @return its hex value
+     */
+    public String toHex(int num) {
+        StringBuilder hexString = new StringBuilder();
+        char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        if (num == 0) return "0";
+        else if (num > 0) {
+            while (num > 0) {
+                hexString.insert(0, hexArray[num & 15]);
+                num >>= 4;
+            }
+        } else {
+            num = -num - 1;
+            for (int i = 0; i < 8; i++) {
+                int index = 15 - (num & 15);
+                hexString.insert(0, hexArray[index]);
+                num >>= 4;
+            }
+        }
+        return hexString.toString();
+    }
 }
