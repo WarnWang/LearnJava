009e7ab68642898494a2e7245dea784709ad9675
Mark Wang <wangyouan0629@hotmail.com>
2016-10-16 14:15:34 +0800

    Solve one question

--- src/LeetCode/Algorithm/DynamicProgramming/Solution3.java
+++ src/LeetCode/Algorithm/DynamicProgramming/Solution3.java
@@ -318,4 +318,33 @@ public class Solution3 {
     private boolean canPartition(int[] nums, int target, int index) {
         return target == 0 || !(index < 0 || target < 0) && (canPartition(nums, target - nums[index], index - 1) || canPartition(nums, target, index - 1));
     }
+
+    /**
+     * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes
+     * the sum of all numbers along its path.
+     * <p>
+     * Note: You can only move either down or right at any point in time.
+     *
+     * @param grid a m * n grid
+     * @return the minimal path
+     */
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            grid[0][i] += grid[0][i - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
 }
