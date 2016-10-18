e200b7b3f51262040068da0bafb094724ae3d64e
Mark Wang <wangyouan0629@hotmail.com>
2016-10-18 09:40:25 +0800

    Add another solution

--- src/LeetCode/Algorithm/Array/Solution2.java
+++ src/LeetCode/Algorithm/Array/Solution2.java
@@ -273,4 +273,50 @@ public class Solution2 {
         }
         return result;
     }
+
+    /**
+     * A sequence of number is called arithmetic if it consists of at least three elements and if the difference between
+     * any two consecutive elements is the same.
+     * <p>
+     * For example, these are arithmetic sequence:
+     * <p>
+     * 1, 3, 5, 7, 9
+     * 7, 7, 7, 7
+     * 3, -1, -5, -9
+     * The following sequence is not arithmetic.
+     * <p>
+     * 1, 1, 2, 5, 7
+     * <p>
+     * A zero-indexed array A consisting of N numbers is given. A slice of that array is any pair of integers (P, Q)
+     * such that 0 <= P < Q < N.
+     * <p>
+     * A slice (P, Q) of array A is called arithmetic if the sequence:
+     * A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+     * <p>
+     * The function should return the number of arithmetic slices in the array A.
+     * <p>
+     * <p>
+     * Example:
+     * <p>
+     * A = [1, 2, 3, 4]
+     * <p>
+     * return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+     *
+     * @param A A zero-indexed array A consisting of N numbers
+     * @return the number of sub sequence that is arithmetic slice
+     */
+    public int numberOfArithmeticSlices(int[] A) {
+        if (A == null || A.length < 3) return 0;
+
+        int sum = 0, count = 0;
+        for (int i = 1, n = A.length - 1; i < n; i++) {
+            if (A[i] - A[i - 1] == A[i + 1] - A[i]) count++;
+            else {
+                sum += count * (count + 1) / 2;
+                count = 0;
+            }
+        }
+        sum += count * (count + 1) / 2;
+        return sum;
+    }
 }
