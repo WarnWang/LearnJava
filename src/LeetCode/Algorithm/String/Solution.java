9a77f9699eb568a13b7fadc66ea4a9ea9c5ae7fa
Mark Wang <wangyouan0629@hotmail.com>
2016-10-30 10:47:09 +0800

    Solve one problem

--- src/LeetCode/Algorithm/String/Solution.java
+++ src/LeetCode/Algorithm/String/Solution.java
@@ -147,4 +147,47 @@ public class Solution {
         }
         return 0;
     }
+
+    /**
+     * Write a program that outputs the string representation of numbers from 1 to n.
+     * <p>
+     * But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output
+     * “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+     * <p>
+     * Example:
+     * <p>
+     * n = 15,
+     * <p>
+     * Return:
+     * [
+     * "1",
+     * "2",
+     * "Fizz",
+     * "4",
+     * "Buzz",
+     * "Fizz",
+     * "7",
+     * "8",
+     * "Fizz",
+     * "Buzz",
+     * "11",
+     * "Fizz",
+     * "13",
+     * "14",
+     * "FizzBuzz"
+     * ]
+     *
+     * @param n number
+     * @return the list of FizzBuzz
+     */
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> fizzBuzzList = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) fizzBuzzList.add("FizzBuzz");
+            else if (i % 3 == 0) fizzBuzzList.add("Fizz");
+            else if (i % 5 == 0) fizzBuzzList.add("Buzz");
+            else fizzBuzzList.add(Integer.toString(i));
+        }
+        return fizzBuzzList;
+    }
 }
