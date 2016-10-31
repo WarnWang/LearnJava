04581111f41f2e5919537b2bcfdd0a914c992e29
Mark Wang <wangyouan0629@hotmail.com>
2016-10-31 10:14:42 +0800

    Solve one problem

--- src/LeetCode/Algorithm/BinaryTree/Solution.java
+++ src/LeetCode/Algorithm/BinaryTree/Solution.java
@@ -330,4 +330,57 @@ public class Solution {
             else return sumNumbers(root.left, pathList, index + 1) + sumNumbers(root.right, pathList, index + 1);
         }
     }
+
+    /**
+     * You are given a binary tree in which each node contains an integer value.
+     * <p>
+     * Find the number of paths that sum to a given value.
+     * <p>
+     * The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from
+     * parent nodes to child nodes).
+     * <p>
+     * The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+     * <p>
+     * Example:
+     * <p>
+     * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+     * <p>
+     * 10
+     * /  \
+     * 5   -3
+     * / \    \
+     * 3   2   11
+     * / \   \
+     * 3  -2   1
+     * <p>
+     * Return 3. The paths that sum to 8 are:
+     * <p>
+     * 1.  5 -> 3
+     * 2.  5 -> 2 -> 1
+     * 3. -3 -> 11
+     *
+     * @param root a binary tree
+     * @param sum  target value
+     * @return target num
+     */
+    public int pathSum(TreeNode root, int sum) {
+        sumNum = 0;
+        pathSum(root, sum, new LinkedList<>());
+        return sumNum;
+    }
+
+    private int sumNum;
+
+    private void pathSum(TreeNode root, int sum, LinkedList<Integer> pathList) {
+        if (root == null) return;
+        int currentValue = (pathList.isEmpty()) ? root.val : pathList.getLast() + root.val;
+        for (int i : pathList) {
+            if (currentValue - i == sum) sumNum++;
+        }
+        if (currentValue == sum) sumNum++;
+        pathList.addLast(currentValue);
+        pathSum(root.left, sum, pathList);
+        pathSum(root.right, sum, pathList);
+        pathList.removeLast();
+    }
 }
